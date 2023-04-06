12f45e3a74a92a4215b4f0fd086c3a54526781f8
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-04-06 14:32:40 -0400

    installed date-fns/subDays to use the :excludeDates. Now some dates are disabled in the calendar but not quit there yet. date -1 is being disabled and i have to manually enter dates to be disabled. Haven' t tested the rest of the app to make sure everything else is still functioning as it should.

--- client/src/components/Appointments/index.jsx
+++ client/src/components/Appointments/index.jsx
@@ -4,17 +4,33 @@ import { ADD_DATE } from "../../utils/mutations";
 import { QUERY_DATES } from '../../utils/queries';
 import DateList from '../DateList';
 import DatePicker from "react-datepicker";
+import subDays from "date-fns/subDays";
+import './index.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css'
+
 
 import "react-datepicker/dist/react-datepicker.css";
-import "react-datepicker/dist/react-datepicker.css";
+import { parseISO } from 'date-fns';
+
 
 const Appointments = () => {
 
     const [startDate, setStartDate] = useState(new Date());
 
+
+
     const [addDate] = useMutation(ADD_DATE);
     const { data } = useQuery(QUERY_DATES);
     const dates = data?.dates || [];
+
+
+    const disableCustomDt = (bookedDays, toBook) => {
+        console.log(bookedDays);
+        console.log(toBook);
+
+        return bookedDays.includes(toBook) ? console.log('date is not available') : console.log(`appointment bookedDays for ${toBook}`);
+
+    };
 
     const bookAppointment = async () => {
         try {
@@ -31,32 +47,39 @@ const Appointments = () => {
 
         for (let i = 0; i < dates.length; i++) {
 
-            const booked = (dates[i].startDate.split('').slice(8, 10).join(''));
-            console.log(booked);
-            const toBook = JSON.stringify(startDate).split('').slice(9, 11).join('');
+            const bookedDays = (dates[i].startDate).split('').slice(0, 10).join('');
+            // console.log(bookedDays);
+            const toBook = JSON.stringify(startDate).split('').slice(1, 11).join('');
             console.log(toBook);
 
-            if (toBook === booked) {
-                console.log('busy at this time');
-                return;
-            };
+            disableCustomDt(bookedDays, toBook);
+            // if (toBook === bookedDays) {
+            //     console.log('busy at this time');
+            //     return;
+            // };
         };
-        console.log('I have time');
-        bookAppointment();
+        // console.log('I have time');
+        // bookAppointment();
     };
     // if (loading) return 'Submitting...';
     // if (error) return `Submission error! ${error.message}`;
+
     return (
         <div className='container'>
             <DatePicker
-                selected={startDate}
+
+                numberOfMonths={2} pagedNavigation
+                // selected={startDate}
+                placeholderText="Click to select a date"
                 onChange={(date) => setStartDate(date)}
                 showTimeSelect
-                placeholderText="Select a weekday"
                 timeFormat="HH:mm"
                 timeIntervals={15}
                 timeCaption="time"
                 dateFormat="MMMM d, yyyy h:mm aa"
+                isValidate={disableCustomDt}
+                excludeDates={[new Date(), subDays(parseISO('2023-04-19'), 1)]}
+            // footer={footer}
             />
             <div>
                 <button type='submit' onClick={() => handleSubmit()}>
