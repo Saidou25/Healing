2ec2ff2c9b95342dc9fecb2aa7eb4940aa801d7e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-04-07 13:19:26 -0400

    setHours from 9am to 7pm. hours are not available in the time section of the calendar. Everything works fine

--- client/src/components/Appointments/index.jsx
+++ client/src/components/Appointments/index.jsx
@@ -1,97 +1,58 @@
 import React, { useState } from 'react';
-import { useQuery } from "@apollo/client";
-// import { ADD_DATE } from "../../utils/mutations";
+import { useQuery, useMutation } from "@apollo/client";
+import { ADD_DATE } from "../../utils/mutations";
 import { QUERY_DATES } from '../../utils/queries';
 import DateList from '../DateList';
 import DatePicker from "react-datepicker";
-// import subDays from "date-fns/subDays";
 import './index.css';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
 
 import "react-datepicker/dist/react-datepicker.css";
-import { parseISO } from 'date-fns';
+import { parseISO, setHours, setMinutes } from 'date-fns';
 
 
 const Appointments = () => {
 
-    const [startDate, setStartDate] = useState(new Date());
+    const [startDate,  setStartDate] = useState(new Date());
+    // const [hours, setHours] = useState();
+    // const [minutes, setMinutes] = useState();
+    // setHours(setMinutes(new Date(), 30), 17)
 
-    // const [addDate] = useMutation(ADD_DATE);
+    const [addDate] = useMutation(ADD_DATE);
     const { data } = useQuery(QUERY_DATES);
+
     const dates = data?.dates || [];
     const allAppointments = [];
+
     for (let i = 0; i < dates.length; i++) {
 
-       const result = ((dates[i].startDate).split('').slice(0, 10).join(''));
-       console.log(result);
-       const resultIso = parseISO(result.toString())
-       console.log(resultIso);
-       allAppointments.push(resultIso);
-    }
-    console.log(allAppointments);
-    // console.log('dates', dates);
-    // console.log('startDate', startDate);
-    // const check = [];
-
-    // const disableCustomDt = (bookedDays, toBook) => {
-    //     console.log('booked days', bookedDays);
-    //     console.log('appointment request', toBook);
-
-    //     if (bookedDays.includes(toBook)) {
-    //         console.log('not available date');
-    //         return;
-    //     } 
-    //     console.log(`available date, congrats your appointment id scheduled on  ${toBook}`);
-    // };
-
-    // const bookAppointment = async () => {
-    //     try {
-    //         await addDate({ variables: { startDate: startDate } });
-    //         console.log(`success adding appointment ${startDate}`);
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // };
-
-    const handleSubmit = () => {
-        // console.log('dates', dates);
-        // console.log('startDate', startDate);
-
-        // for (let i = 0; i < dates.length; i++) {
-
-        // const bookedDays = (dates[i].startDate).split('').slice(0, 10).join('');
-        console.log('hello');
-        // const toBook = JSON.stringify(startDate).split('').slice(1, 11).join('');
-        // console.log(toBook);
-
-        // disableCustomDt(bookedDays, toBook);
-        // if (toBook === bookedDays) {
-        //     console.log('busy at this time');
-        //     return;
-        // };
-        // };
-        // console.log('I have time');
-        // bookAppointment();
+        const result = ((dates[i].startDate).split('').slice(0, 10).join(''));
+        const resultIso = parseISO(result.toString());
+        allAppointments.push(resultIso);
     };
-    // if (loading) return 'Submitting...';
-    // if (error) return `Submission error! ${error.message}`;
+
+    const handleSubmit = async () => {
+        try {
+            await addDate({ variables: { startDate: startDate } });
+            console.log(`success adding appointment ${startDate}`);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     return (
         <div className='container'>
             <DatePicker
-
-                numberOfMonths={2} pagedNavigation
                 selected={startDate}
-                // placeholderText="Click to select a date"
                 onChange={(date) => setStartDate(date)}
                 showTimeSelect
                 timeFormat="HH:mm"
                 timeIntervals={15}
                 timeCaption="time"
+                minTime={setHours(setMinutes(new Date(), 0), 9)}
+                maxTime={setHours(setMinutes(new Date(), 30), 19)}
                 dateFormat="MMMM d, yyyy h:mm aa"
-                // isValidate={disableCustomDt}
                 excludeDates={allAppointments}
-
             // footer={footer}
             />
             <div>
