d137e70cf997376460dabff0ae3ea2620584da37
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-04-07 11:17:30 -0400

    did it! :-) . previously booked appointments are now showing greyed in the calendar. These dates are disabled.

--- client/src/components/Appointments/index.jsx
+++ client/src/components/Appointments/index.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import { useQuery, useMutation } from "@apollo/client";
-import { ADD_DATE } from "../../utils/mutations";
+import { useQuery } from "@apollo/client";
+// import { ADD_DATE } from "../../utils/mutations";
 import { QUERY_DATES } from '../../utils/queries';
 import DateList from '../DateList';
 import DatePicker from "react-datepicker";
-import subDays from "date-fns/subDays";
+// import subDays from "date-fns/subDays";
 import './index.css';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
-
 
 import "react-datepicker/dist/react-datepicker.css";
 import { parseISO } from 'date-fns';
@@ -17,47 +16,60 @@ const Appointments = () => {
 
     const [startDate, setStartDate] = useState(new Date());
 
-
-
-    const [addDate] = useMutation(ADD_DATE);
+    // const [addDate] = useMutation(ADD_DATE);
     const { data } = useQuery(QUERY_DATES);
     const dates = data?.dates || [];
+    const allAppointments = [];
+    for (let i = 0; i < dates.length; i++) {
 
+       const result = ((dates[i].startDate).split('').slice(0, 10).join(''));
+       console.log(result);
+       const resultIso = parseISO(result.toString())
+       console.log(resultIso);
+       allAppointments.push(resultIso);
+    }
+    console.log(allAppointments);
+    // console.log('dates', dates);
+    // console.log('startDate', startDate);
+    // const check = [];
 
-    const disableCustomDt = (bookedDays, toBook) => {
-        console.log(bookedDays);
-        console.log(toBook);
+    // const disableCustomDt = (bookedDays, toBook) => {
+    //     console.log('booked days', bookedDays);
+    //     console.log('appointment request', toBook);
 
-        return bookedDays.includes(toBook) ? console.log('date is not available') : console.log(`appointment bookedDays for ${toBook}`);
+    //     if (bookedDays.includes(toBook)) {
+    //         console.log('not available date');
+    //         return;
+    //     } 
+    //     console.log(`available date, congrats your appointment id scheduled on  ${toBook}`);
+    // };
 
-    };
-
-    const bookAppointment = async () => {
-        try {
-            await addDate({ variables: { startDate: startDate } });
-            console.log(`success adding appointment ${startDate}`);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+    // const bookAppointment = async () => {
+    //     try {
+    //         await addDate({ variables: { startDate: startDate } });
+    //         console.log(`success adding appointment ${startDate}`);
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
 
     const handleSubmit = () => {
-        console.log('dates', dates);
-        console.log('startDate', startDate);
+        // console.log('dates', dates);
+        // console.log('startDate', startDate);
 
-        for (let i = 0; i < dates.length; i++) {
+        // for (let i = 0; i < dates.length; i++) {
 
-            const bookedDays = (dates[i].startDate).split('').slice(0, 10).join('');
-            // console.log(bookedDays);
-            const toBook = JSON.stringify(startDate).split('').slice(1, 11).join('');
-            console.log(toBook);
+        // const bookedDays = (dates[i].startDate).split('').slice(0, 10).join('');
+        console.log('hello');
+        // const toBook = JSON.stringify(startDate).split('').slice(1, 11).join('');
+        // console.log(toBook);
 
-            disableCustomDt(bookedDays, toBook);
-            // if (toBook === bookedDays) {
-            //     console.log('busy at this time');
-            //     return;
-            // };
-        };
+        // disableCustomDt(bookedDays, toBook);
+        // if (toBook === bookedDays) {
+        //     console.log('busy at this time');
+        //     return;
+        // };
+        // };
         // console.log('I have time');
         // bookAppointment();
     };
@@ -69,16 +81,17 @@ const Appointments = () => {
             <DatePicker
 
                 numberOfMonths={2} pagedNavigation
-                // selected={startDate}
-                placeholderText="Click to select a date"
+                selected={startDate}
+                // placeholderText="Click to select a date"
                 onChange={(date) => setStartDate(date)}
                 showTimeSelect
                 timeFormat="HH:mm"
                 timeIntervals={15}
                 timeCaption="time"
                 dateFormat="MMMM d, yyyy h:mm aa"
-                isValidate={disableCustomDt}
-                excludeDates={[new Date(), subDays(parseISO('2023-04-19'), 1)]}
+                // isValidate={disableCustomDt}
+                excludeDates={allAppointments}
+
             // footer={footer}
             />
             <div>
