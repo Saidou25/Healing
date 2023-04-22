358e27da2b982fabebc7fe7eff4c517e7aa77dd4
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-04-22 08:01:39 -0400

    Early morning landing page styling

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -4,18 +4,27 @@ import './index.css';
 
 const LandingPage = () => {
     return (
-        <div className="main">
-            <h1>Reiki</h1>
-            <div className="row">
-                <div className="col-6">
-                    <button className="btn visit" >
-                        <Link to='/Login' className='visit-item'>Login</Link>
-                    </button>
-                </div>
-                <div className="col-6">
-                    <button className="btn visit" >
-                        <Link to='/Signup' className='visit-item'>signup</Link>
-                    </button>
+        <div className="mainlandingpage">
+            <h1 className='reiki text-white m-4' style={{ fontSize: '7rem' }}>Reiki</h1>
+            <div className="landing-parent">
+                <div className="card landing-page">
+                    <div className="row landing-row">
+                        <div className="col-4 landig-column">
+                            <button className="btn but-landing" >
+                                <Link to='/Login' className='visit-item'>Login</Link>
+                            </button>
+                        </div>
+                        <div className="col-4 landig-column">
+                            <button className="btn but-landing" >
+                                <Link to='/Signup' className='visit-item'>signup</Link>
+                            </button>
+                        </div>
+                        <div className="col-4 landig-column">
+                            <button className="btn but-landing" >
+                                <Link to='/Visit' className='visit-item'>Visit</Link>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
