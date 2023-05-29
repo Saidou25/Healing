b0ab14ea53e10bf8d2638ac56164902b7dae0bea
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-05-29 12:52:24 -0400

    reverted

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Healing',
+  process.env.MONGODB_URI || 'mongodb://localhost/healing',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
