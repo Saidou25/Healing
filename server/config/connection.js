d0bda426780fe590c90edf6212e9161448318914
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-05-30 21:00:37 -0400

    127.0.0.1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/healing',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1/healing',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
