d58646be6388a847ca467607d67a5af6687c2228
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-06-02 07:57:08 -0400

    3001

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/healing',
+  process.env.MONGODB_URI || 3001,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
