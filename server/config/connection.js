5b9907c432ada98cdd5f4a2e438e7f7521d81aa3
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-06-02 08:04:51 -0400

    didn't save

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 3001,
+  process.env.MONGODB_URI || 'mongodb://localhost/healing',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
