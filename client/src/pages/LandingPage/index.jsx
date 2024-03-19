465eaf839ab4b456e9490710ca1e8ba0c4aa1786
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-03-19 13:50:51 -0400

    changing landingPage design

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -1,44 +1,40 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import SideMenu from "../../components/SideMenu";
-import './index.css';
+import "./index.css";
 
 const LandingPage = () => {
-
-    return (
-        <div className="mainlandingpage">
-            <div className="row">
-                <div className='col-12 d-flex justify-content-end'>
-                    <SideMenu />
-                </div>
-            </div>
-            <div className="landing-title">
-                <NavLink className="col-12 landing text-black" to="/">
-                    Healing
-                </NavLink>
-            </div>
-            <div className="landing-parent">
-                <div className="card landing-page rounded-0">
-                    <div className="row landing-row">
-                        <div className="col-4 landing-column">
-                            <button className="btn but-landing" >
-                                <NavLink to='/Login' className='landing-item'>Login</NavLink>
-                            </button>
-                        </div>
-                        <div className="col-4 landing-column">
-                            <button className="btn but-landing" >
-                                <NavLink to='/Signup' className='landing-item'>Signup</NavLink>
-                            </button>
-                        </div>
-                        <div className="col-4 landing-column">
-                            <button className="btn but-landing" >
-                                <NavLink to='/About' className='landing-item'>About</NavLink>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="mainlandingpage">
+      <div className="row">
+        <div className="col-12 d-flex justify-content-end">
+          <SideMenu />
         </div>
-    )
+      </div>
+      <div className="landing-title">
+        <NavLink className="col-12 landing text-light" to="/">
+          Healing
+        </NavLink>
+      </div>
+      <div className="row landing-row py-5">
+        <div className="col-12 landing-column">
+          <NavLink to="/Login" className="landing-item"
+          >
+            Login
+          </NavLink>
+        </div>
+        <div className="col-12 landing-column mt-3">
+          <NavLink to="/Signup" className="landing-item">
+            Signup
+          </NavLink>
+        </div>
+        <div className="col-12 landing-column mt-3">
+          <NavLink to="/About" className="landing-item">
+            About
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LandingPage;
